95a6e5b033bbbfb474cbbe2fa3209342b3823b8a
Revan Sarbast Salih <revansarbast8@gmail.com>
2022-10-06 23:36:49 +0300

    add: more components
    adding more components
    
    implementing GetStarted component
    
    implementing Hero component
    
    implementing stats component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
 import styles from "./style";
 
+import {
+  Billing,
+  Business,
+  CardDeal,
+  Clients,
+  CTA,
+  Footer,
+  Testimonials,
+  Navbar,
+  Hero,
+  Stats,
+} from "./components";
+
 function App() {
   return (
     <div className="w-full overflow-hidden bg-primary">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart} `}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart} `}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
       </div>
     </div>
   );
